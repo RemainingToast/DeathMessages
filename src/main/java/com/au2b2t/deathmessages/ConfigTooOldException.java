bf2eab624550a0d277032d385971d50bf97922ed
RemainingToast <59129680+RemainingToast@users.noreply.github.com>
2021-05-16 21:28:22 +1000

    Add files via upload

---
+++ src/main/java/com/au2b2t/deathmessages/ConfigTooOldException.java
@@ -0,0 +1,10 @@
+package com.au2b2t.deathmessages;
+
+/**
+ * Placeholder exception for detecting old configurations
+ */
+public class ConfigTooOldException extends Exception {
+
+    private static final long serialVersionUID = 6955844502576959492L;
+
+}
---
+++ src/main/java/com/au2b2t/deathmessages/DMPReloadEvent.java
@@ -0,0 +1,25 @@
+package com.au2b2t.deathmessages;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ * An event that is guaranteed to trigger on every player death before any other DMP events are triggered or any formatTag functions called.
+ *
+ */
+public class DMPReloadEvent extends Event {
+    private static final HandlerList handlers = new HandlerList();
+    
+    public DMPReloadEvent() {
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+}
